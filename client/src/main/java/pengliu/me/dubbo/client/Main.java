2b5e835678fdfe31877b64aa83fa4460df30e260
liupeng330 <330liupeng@gmail.com>
2015-12-02 15:30:57 +0800

    12

--- client/src/main/java/pengliu/me/dubbo/client/Main.java
+++ client/src/main/java/pengliu/me/dubbo/client/Main.java
@@ -1,24 +1,19 @@
 package pengliu.me.dubbo.client;
 
+import com.heika.verify.flow.bean.VerifyUserStatusVo;
+import com.heika.verify.flow.inerface.IVerifyFlowService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import pengliu.me.dubbo.service.DemoService;
-
-import java.util.Date;
 
 public class Main
 {
     public static void main(String[] args) throws Exception
     {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"applicationConsumer.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"applicationConsumer_flowTask.xml"});
         context.start();
         System.out.println("Start to getBean...");
-        DemoService demoService = (DemoService) context.getBean("demoService");
-        while(true)
-        {
-            System.out.println("Start to invoking...");
-            demoService.sayHello("From client, " + new Date());
-            Thread.sleep(1000);
-            System.out.println("End invoking...");
-        }
+        IVerifyFlowService flowTaskService = (IVerifyFlowService) context.getBean("flowService");
+        flowTaskService.initVerifyUserStatus(100034834);
+        flowTaskService.commitVerifyUserStatus(100034834);
+        //VerifyUserStatusVo verifyUserStatusVo = flowTaskService.loadVerifyUserStatusByUserId(100034635);
     }
 }
